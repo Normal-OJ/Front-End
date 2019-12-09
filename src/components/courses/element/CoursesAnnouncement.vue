abb14712fd56be831650da99d3f7b3825c5bd9fe
Uier <ap9940506@gmail.com>
2019-12-09 22:39:41 +0800

    elements of courses

---
+++ src/components/courses/element/CoursesAnnouncement.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'CoursesAnnouncement',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/courses/element/CoursesDiscussion.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'CoursesDiscussion',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
