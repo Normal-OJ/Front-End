ff6bff7958c80bb6796e66416262d0f9cf5fe265
鐘子淳 <40747030S@ntnu.edu.tw>
2019-12-10 19:39:47 +0800

    switch folder & mail

--- src/components/inbox/Inbox.vue
+++ src/components/inbox/Inbox.vue
@@ -18,10 +18,10 @@
         <v-list nav>
           <v-list-item-group color="primary">
             <v-list-item 
-              v-for="item in navbar"
-              :key="item.title"
-              link
+              v-for="(item, i) in navbar"
+              :key="i"
               dense
+              @click="clear(i)"
             >
               <v-list-item-icon>
                 <v-icon>{{item.icon}}</v-icon>
@@ -35,7 +35,8 @@
       </v-card>
     </v-col>
     <v-col cols="3">
-      <v-card tile outlined height="93vh">
+      <v-card tile elevation="0" outlined height="93vh">
+        <v-card tile height="92.5%">
           <v-card-title>
             <v-text-field
               outlined
@@ -43,46 +44,49 @@
               label="Search author, subject ..."
               hide-details
               @click:append=""
-          ></v-text-field>
-        </v-card-title>
-        <v-divider></v-divider>
-        <v-list 
-          two-line
-          v-for="(item, i) in mail"
-          :key="i"
-          class="pa-0"
-        >
-          <v-list-item @click="displaymail = i">
-            <v-list-item-avatar>
-              <v-img :src="item.avatar"></v-img>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>{{item.subject}}</v-list-item-title>
-              <v-list-item-subtitle>{{item.sender}} - {{item.time}}</v-list-item-subtitle>
-              <v-list-item-subtitle>{{item.content}}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
+            ></v-text-field>
+          </v-card-title>
           <v-divider></v-divider>
-        </v-list>
-        <v-divider></v-divider>
-        <v-pagination
-          :length="2"
-          v-model="page"
-        ></v-pagination>
+          <v-list 
+            two-line
+            v-for="(item, i) in mail[displayfolder].folder"
+            :key="i"
+            class="pa-0"
+          >
+            <v-list-item @click="displaymail = i">
+              <v-list-item-avatar>
+                <v-img :src="item.avatar"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title>{{item.subject}}</v-list-item-title>
+                <v-list-item-subtitle>{{item.sender}} - {{item.time}}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{item.content}}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider></v-divider>
+          </v-list>
+        </v-card>
+        <v-card tile elevation="0" height="7.5%">
+          <v-pagination
+            :length="2"
+            v-model="page"
+            v-if="displayfolder != -1"
+          ></v-pagination>
+        </v-card>
       </v-card>
     </v-col>
     <v-col cols="7" class="ma-0">
-      <v-card tile elevation="0" height="93vh">
-        <v-card tile outlined height="20%" class="pt-3 px-3">
+      <v-card tile outlined height="93vh">
+        <v-card tile elevation="0" height="20%" class="pt-3 px-3">
           <v-list-item v-if="displaymail === -1"></v-list-item>
           <v-list-item v-else three-line>
             <v-list-item-avatar size="64px">
-              <v-img :src="mail[displaymail].avatar"></v-img>
+              <v-img :src="mail[displayfolder].folder[displaymail].avatar"></v-img>
             </v-list-item-avatar>
             <v-list-item-content>
-              <v-list-item-title class="headline">{{mail[displaymail].sender}}</v-list-item-title>
+              <v-list-item-title class="headline">{{mail[displayfolder].folder[displaymail].sender}}</v-list-item-title>
               <v-list-item-subtitle>From "Computer Programming II"</v-list-item-subtitle>
-              <v-list-item-subtitle>{{mail[displaymail].time}}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{mail[displayfolder].folder[displaymail].time}}</v-list-item-subtitle>
             </v-list-item-content>
             <v-spacer></v-spacer>
             <v-list-item-icon>
@@ -95,7 +99,8 @@
             </v-list-item-icon>
           </v-list-item>
         </v-card>
-        <v-card tile outlined height="70%">
+        <v-divider></v-divider>
+        <v-card tile elevation="0" height="70%">
           <v-list-item v-if="displaymail === -1">
             <v-list-item-content>
               <v-icon x-large color="grey">mdi-email</v-icon>
@@ -103,15 +108,16 @@
             </v-list-item-content>
           </v-list-item>
           <v-list-item v-else>
-            <v-list-item-content>
-              <v-list-item-title class="headline">{{mail[displaymail].subject}}</v-list-item-title>
-              <v-list-item-subtitle class="black--text mt-2 text-wrap">{{mail[displaymail].content}}</v-list-item-subtitle>
+            <v-list-item-content class="ma-3">
+              <v-list-item-title class="headline">{{mail[displayfolder].folder[displaymail].subject}}</v-list-item-title>
+              <v-list-item-subtitle class="black--text text-wrap">{{mail[displayfolder].folder[displaymail].content}}</v-list-item-subtitle>
             </v-list-item-content>
           </v-list-item>
         </v-card>
-        <v-card tile outlined height="10%">
+        <v-divider></v-divider>
+        <v-card tile elevation="0" height="10%">
           <v-list-item v-if="displaymail === -1"></v-list-item>
-          <v-list-item>
+          <v-list-item v-else>
             <v-list-item-subtitle>Attachment</v-list-item-subtitle>
           </v-list-item>
         </v-card>
@@ -127,6 +133,9 @@ export default {
 
   data () {
     return {
+      displayfolder: 0,
+      displaymail: -1,
+      page: 1,
     	navbar: [
     		{ icon: 'mdi-inbox', title: 'Inbox'},
     		{ icon: 'mdi-send', title: 'Sent'},
@@ -134,28 +143,88 @@ export default {
     		{ icon: 'mdi-delete', title: 'Trash'}
     	],
       mail: [
-        { sender: 'Uier',
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          subject: '資工營面試通知',
-          content: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
-          time: '2019/03/05 03:05'
+        {
+          // inbox
+          folder: [
+            { 
+              sender: 'Uier',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: '資工營面試通知',
+              content: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              time: '2019/03/05 23:32'
+            },
+            { 
+              sender: 'Kelly',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: 'test',
+              content: '資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面',
+              time: '2019/12/13 12:13'
+            },
+            { 
+              sender: 'ysl',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: 'ooo',
+              content: '0991234567890qwer tyuioasdfghjklzxcvbnm,098765edkdftgyhunfd6csxexbabiubcuibgfwvgurefviuegrvfuervfuwrvfiwugvfuevfugrvfuwvrfuviugfevrugfvrguvurefviuegrvfuervfuwrvfiwugvfuevfugrvfuwvrfuviugfevrugfvrguvurefviuegrvfuervfuwrvfiwugvfuevfugrvfuwvrfuviugfevrugfvrguv',
+              time: '2019/05/11 05:11'
+            },
+            { 
+              sender: '黃欣欣',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: '資工營面試通知',
+              content: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              time: '2019/11/02 11:02'
+            },
+            { 
+              sender: '臻臻',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: '資工營面試通知',
+              content: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              time: '2019/04/06 04:06'
+            },
+            { 
+              sender: 'ooo',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: '資工營面試通知',
+              content: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              time: '2019/03/05 23:32'
+            },
+            
+          ]
         },
-        { sender: 'cpj',
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          subject: 'test',
-          content: '資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面',
-          time: '2019/12/13 12:13'
+        { // sent
+          folder: [
+            {
+              sender: 'cpj',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: 'iiiiiiiiii',
+              content: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              time: '2019/09/02 09:02'
+            }
+          ]
         },
-        { sender: 'aflafl',
-          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          subject: 'ooo',
-          content: '0991234567890qwer tyuioasdfghjklzxcvbnm,098765edkdftgyhunfd6csxexbabiubcuibgfwvgurefviuegrvfuervfuwrvfiwugvfuevfugrvfuwvrfuviugfevrugfvrguvurefviuegrvfuervfuwrvfiwugvfuevfugrvfuwvrfuviugfevrugfvrguvurefviuegrvfuervfuwrvfiwugvfuevfugrvfuwvrfuviugfevrugfvrguv',
-          time: '2019/05/11 05:11'
+        { //draft
+          folder: [
+          ]
+        },
+        { //trash
+          folder: [
+            {
+              sender: 'ruei',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: 'iiiiiiiiii',
+              content: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              time: '2019/11/08 11:08'
+            }
+          ]
         }
       ],
-      displaymail: -1,
-      page: 1
     }
+  },
+  methods: {
+    clear(i) {
+      this.displayfolder = i
+      this.displaymail = -1
+    },
   }
 }
 </script>
