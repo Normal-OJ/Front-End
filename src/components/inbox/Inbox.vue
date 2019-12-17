ebf277d9aec11778588b599b48d3fbd8b4afbad6
鐘子淳 <40747030S@ntnu.edu.tw>
2019-12-17 09:37:46 +0800

    remove draft.trash & RWD without mail content

--- src/components/inbox/Inbox.vue
+++ src/components/inbox/Inbox.vue
@@ -1,12 +1,12 @@
 <template>
   <v-row no-gutters>
-    <v-col cols="2">
+    <v-col cols="2" class="hidden-sm-and-down">
       <v-card tile outlined height="93vh">
         <v-row justify="center" align="center"><v-col cols="4" class="pl-6">
-          <v-dialog v-model="composeDialog" persistent width="50%">
+          <v-dialog v-model="composeDialog" scrollable width="80%">
             <template v-slot:activator="{ on }">
               <v-btn color="primary" fab v-on="on" small>
-                <v-icon>mdi-plus-thick</v-icon>
+                <v-icon>mdi-pencil</v-icon>
               </v-btn>
             </template>
             <v-card>
@@ -15,31 +15,23 @@
                 <v-spacer></v-spacer>
                 <v-tooltip bottom>
                   <template v-slot:activator="{ on }">
-                    <v-btn tile icon @click="ToDraft()" v-on="on"><v-icon>mdi-close</v-icon></v-btn>
+                    <v-btn tile icon @click="ResetNewmail()" v-on="on"><v-icon>mdi-close</v-icon></v-btn>
                   </template>
-                  <span>save to Draft and close</span>
+                  <span>close</span>
                 </v-tooltip>
               </v-toolbar>
-              <v-container>
+              <v-card-text>
                 <v-text-field label="To" required hint="enter an username" v-model="draftSentTo"></v-text-field>
                 <v-text-field label="Subject" required v-model="draftSubject"></v-text-field>
-                <v-textarea label="Message" persistent-hint hint="the draft won't be save if 'Message' is blank" v-model="draftMessage"></v-textarea>
+                <v-textarea label="Message" v-model="draftMessage"></v-textarea>
                 <v-row>
                   <v-spacer></v-spacer>
                   <v-card-actions>
-                    <v-btn
-                      outlined
-                      color="primary"
-                      @click="ResetNewmail()"
-                    >cancel</v-btn>
-                    <v-btn
-                      dark
-                      color="primary"
-                      @click="composeDialog = false"
-                    >send</v-btn>
+                    <v-btn outlined color="primary" @click="ResetNewmail()">cancel</v-btn>
+                    <v-btn dark color="primary" @click="composeDialog = false">send</v-btn>
                   </v-card-actions>
                 </v-row>
-              </v-container>
+              </v-card-text>
             </v-card>
           </v-dialog></v-col>
           <v-col><div class="subtitle-1">Compose</div></v-col>
@@ -60,48 +52,86 @@
         </v-list>
       </v-card>
     </v-col>
-    <v-col cols="3">
+    <v-col cols="12" md="3">
       <v-card tile elevation="0" outlined height="93vh">
-        <v-card tile elevation="0" height="92.5%">
-          <v-card-title>
-            <v-text-field
-              outlined
-              append-icon="mdi-magnify"
-              label="Search author, subject ..."
-              hide-details
-              @click:append=""
-            ></v-text-field>
-          </v-card-title>
+        <v-card-title>
+          <v-text-field
+            outlined
+            append-icon="mdi-magnify"
+            label="Search author, subject ..."
+            hide-details
+            @click:append=""
+          ></v-text-field>
+        </v-card-title>
+        <v-divider></v-divider>
+        <v-list 
+          two-line
+          v-for="(item, i) in mail[displayfolder].folder"
+          :key="item.time"
+          class="pa-0"
+        >
+          <v-list-item @click="Click(i)">
+            <v-list-item-avatar>
+              <v-img :src="item.avatar"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>{{item.subject}}</v-list-item-title>
+              <v-list-item-subtitle>{{item.sender}} - {{item.time}}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{item.content}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
           <v-divider></v-divider>
-          <v-list 
-            two-line
-            v-for="(item, i) in mail[displayfolder].folder"
-            :key="item.time"
-            class="pa-0"
-          >
-            <v-list-item @click="displaymail = i">
-              <v-list-item-avatar>
-                <v-img :src="item.avatar"></v-img>
+        </v-list>
+        <v-dialog v-model="contentDialog" id="dialog">
+          <v-card height="80vh">
+            <v-list-item three-line>
+              <!-- <v-list-item-avatar size="64px">
+                <v-img :src="mail[displayfolder].folder[displaymail].avatar"></v-img>
               </v-list-item-avatar>
               <v-list-item-content>
-                <v-list-item-title>{{item.subject}}</v-list-item-title>
-                <v-list-item-subtitle>{{item.sender}} - {{item.time}}</v-list-item-subtitle>
-                <v-list-item-subtitle>{{item.content}}</v-list-item-subtitle>
-              </v-list-item-content>
+                <v-list-item-title class="headline">{{mail[displayfolder].folder[displaymail].sender}}</v-list-item-title>
+                <v-list-item-subtitle>{{mail[displayfolder].folder[displaymail].time}}</v-list-item-subtitle>
+              </v-list-item-content> -->
+              <v-spacer></v-spacer>
+              <v-tooltip bottom v-if="displayfolder != 1">
+                <template v-slot:activator="{ on }">
+                  <v-btn class="ml-1" icon outlined v-on="on">
+                    <v-icon @click="Reply(displayfolder,displaymail)">mdi-reply</v-icon>
+                  </v-btn>
+                </template>
+                <span>reply</span>
+              </v-tooltip>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                  <v-btn class="ml-1" icon outlined v-on="on" @click="Remove(displayfolder,displaymail)">
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </template>
+                <span>delete</span>
+              </v-tooltip>
             </v-list-item>
-            <v-divider></v-divider>
-          </v-list>
-        </v-card>
-        <v-card tile elevation="0" height="7.5%">
-          <v-pagination
-            :length="2"
-            v-model="page"
-            v-if="displayfolder != -1"
-          ></v-pagination>
-        </v-card>
+          </v-card>
+        </v-dialog>
+        <v-btn left bottom fixed fab class="text-center hidden-md-and-up" color="primary" @click="composeDialog = true">
+          <v-icon>mdi-pencil</v-icon>
+        </v-btn>
+        <v-speed-dial right fixed bottom v-model="speedDial" class="hidden-md-and-up">
+          <template v-slot:activator>
+            <v-btn v-model="speedDial" color="primary" dark fab>
+              <v-icon v-if="speedDial">mdi-close</v-icon>
+              <v-icon v-else>mdi-folder-open</v-icon>
+            </v-btn>
+          </template>
+          <v-btn fab small color="grey--text" @click="displayfolder = 1">
+            <v-icon>mdi-send</v-icon>
+          </v-btn>
+          <v-btn fab small color="grey--text" @click="displayfolder = 0">
+            <v-icon>mdi-inbox</v-icon>
+          </v-btn>
+        </v-speed-dial>
       </v-card>
     </v-col>
-    <v-col cols="7" class="ma-0">
+    <v-col cols="7" class="ma-0 hidden-sm-and-down">
       <v-card tile outlined height="93vh">
         <v-card tile elevation="0" height="20%" class="pt-3 px-3">
           <v-list-item v-if="displaymail === -1"></v-list-item>
@@ -111,21 +141,16 @@
             </v-list-item-avatar>
             <v-list-item-content>
               <v-list-item-title class="headline">{{mail[displayfolder].folder[displaymail].sender}}</v-list-item-title>
-              <v-list-item-subtitle>From "Computer Programming II"</v-list-item-subtitle>
               <v-list-item-subtitle>{{mail[displayfolder].folder[displaymail].time}}</v-list-item-subtitle>
             </v-list-item-content>
             <v-spacer></v-spacer>
             <v-tooltip bottom v-if="displayfolder != 1">
               <template v-slot:activator="{ on }">
                 <v-btn class="ml-1" icon outlined v-on="on">
-                  <v-icon v-if="displayfolder === 2" @click="EditDraft(2)">mdi-pencil-outline</v-icon>
-                  <v-icon v-else-if="displayfolder === 3" @click="Move(0)">mdi-inbox-arrow-down</v-icon>
-                  <v-icon v-else @click="EditDraft(0)">mdi-reply</v-icon>
+                  <v-icon @click="Reply(displayfolder,displaymail)">mdi-reply</v-icon>
                 </v-btn>
               </template>
-              <span v-if="displayfolder === 0">reply</span>
-              <span v-else-if="displayfolder === 2">edit</span>
-              <span v-else-if="displayfolder === 3">move back to inbox</span>
+              <span>reply</span>
             </v-tooltip>
             <v-tooltip bottom>
               <template v-slot:activator="{ on }">
@@ -133,8 +158,7 @@
                   <v-icon>mdi-delete</v-icon>
                 </v-btn>
               </template>
-              <span v-if="displayfolder === 0 || displayfolder === 1">move to trash</span>
-              <span v-else>delete</span>
+              <span>delete</span>
             </v-tooltip>
           </v-list-item>
         </v-card>
@@ -176,14 +200,13 @@ export default {
       draftSubject: '',
       draftMessage: '',
       composeDialog: false,
+      contentDialog: false,
       displayfolder: 0,
       displaymail: -1,
-      page: 1,
+      speedDial: false,
     	navbar: [
     		{ icon: 'mdi-inbox', title: 'Inbox'},
     		{ icon: 'mdi-send', title: 'Sent'},
-    		{ icon: 'mdi-file', title: 'Draft'},
-    		{ icon: 'mdi-delete', title: 'Trash'}
     	],
       mail: [
         {
@@ -244,53 +267,24 @@ export default {
               time: '2019/09/02 09:02'
             }
           ]
-        },
-        { //draft
-          folder: [
-          ]
-        },
-        { //trash
-          folder: [
-            {
-              sender: 'ruei',
-              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-              subject: 'iiiiiiiiii',
-              message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
-              time: '2019/11/08 11:08'
-            }
-          ]
         }
       ],
     }
   },
   methods: {
+    Click(i) {
+      this.contentDialog = true
+      this.displaymail = i
+    },
     Clear(i) {
       this.displayfolder = i
       this.displaymail = -1
     },
     Remove(f,idx) {
-      if(f === 0 || f === 1) {
-        this.mail[3].folder.splice(0, 0, this.mail[f].folder[idx])
-      } //move to trash
       this.mail[f].folder.splice(idx, 1)
       if(idx === this.mail[f].folder.length) {
         this.displaymail--
       }
-    },
-    ToDraft() {
-      if(this.displayfolder == 2){
-        this.mail[2].folder[this.displaymail].sender = this.draftSentTo
-        this.mail[2].folder[this.displaymail].subject = this.draftSubject
-        this.mail[2].folder[this.displaymail].message = this.draftMessage
-      } //update the draft
-      else if(this.draftMessage != ''){
-        this.mail[2].folder.push({sender: this.draftSentTo, subject: this.draftSubject, message: this.draftMessage})
-      } //push to draft
-      this.ResetNewmail()
-    },
-    Move(to) {
-      this.mail[to].folder.push(this.mail[3].folder[this.displaymail])
-      this.Remove(3,this.displaymail)
     },
     ResetNewmail() {
       this.draftSentTo = ''
@@ -298,16 +292,33 @@ export default {
       this.draftMessage = ''
       this.composeDialog = false
     },
-    EditDraft(f) {
-      if(f === 0) this.draftSubject += 'Re:'
-      this.draftSentTo = this.mail[f].folder[this.displaymail].sender
-      this.draftSubject += this.mail[f].folder[this.displaymail].subject
-      if(f === 2) this.draftMessage = this.mail[f].folder[this.displaymail].message
-      this.composeDialog = !this.composeDialog
+    Reply(f,m) {
+      this.draftSentTo = this.mail[f].folder[m].sender
+      this.draftSubject = 'Re: ' + this.mail[f].folder[m].subject
+      this.composeDialog = true
     }
   }
 }
 </script>
 
 <style lang="css" scoped>
+
+@media screen and (max-width: 960px) {
+  #dialog {
+    visibility: visible;
+  }
+}
+
+@media screen and (min-width: 960px) and (max-width: 1904px) {
+  #dialog {
+    visibility: hidden;
+  }
+}
+
+@media screen and (min-width: 1904px) {
+  #dialog {
+    visibility: hidden;
+  }
+}
+
 </style>
