d8e8761cff9e38cea982735070178c219d67e353
鐘子淳 <40747030S@ntnu.edu.tw>
2019-12-19 12:58:30 +0800

    finish RWD & read/unread

--- src/components/inbox/Inbox.vue
+++ src/components/inbox/Inbox.vue
@@ -20,7 +20,7 @@
                   <span>close</span>
                 </v-tooltip>
               </v-toolbar>
-              <v-card-text>
+              <v-card-text class="mt-2">
                 <v-text-field label="To" required hint="enter an username" v-model="draftSentTo"></v-text-field>
                 <v-text-field label="Subject" required v-model="draftSubject"></v-text-field>
                 <v-textarea label="Message" v-model="draftMessage"></v-textarea>
@@ -53,8 +53,49 @@
       </v-card>
     </v-col>
     <v-col cols="12" md="3">
-      <v-card tile elevation="0" outlined height="93vh">
-        <v-card-title>
+      <v-slide-x-reverse-transition><v-card tile elevation="0" outlined v-if="showMail && displaymail !== -1" height="93vh" class="hidden-md-and-up">
+        <v-card tile elevation="0" height="25%" class="pt-3 px-3">
+          <v-card-title class="pa-0">
+            <v-btn icon @click="showMail = false"><v-icon large>mdi-chevron-left</v-icon></v-btn>
+            <v-spacer></v-spacer>
+          </v-card-title>
+          <v-list-item two-line>
+            <v-list-item-avatar size="64px">
+              <v-img :src="mail[displayfolder].folder[displaymail].avatar"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title class="headline">{{mail[displayfolder].folder[displaymail].sender}}</v-list-item-title>
+              <v-list-item-subtitle>{{mail[displayfolder].folder[displaymail].timestamp}}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-btn class="ma-1" icon outlined v-if="displayfolder === 0"><v-icon @click="Reply(displayfolder,displaymail)">mdi-reply</v-icon></v-btn>
+            <v-btn class="ma-1" icon outlined @click="Remove(displayfolder,displaymail)"><v-icon>mdi-delete</v-icon></v-btn>
+          </v-list-item>
+        </v-card>
+        <v-divider></v-divider>
+        <v-card tile elevation="0" height="60%" class="overflow-y-auto">
+          <v-list-item v-if="displaymail === -1">
+            <v-list-item-content>
+              <v-icon x-large color="grey">mdi-email</v-icon>
+              <v-list-item-subtitle class="text-center">Empty</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item v-else>
+            <v-list-item-content class="ma-3">
+              <v-list-item-title class="headline">{{mail[displayfolder].folder[displaymail].subject}}</v-list-item-title>
+              <v-list-item-subtitle class="black--text text-wrap">{{mail[displayfolder].folder[displaymail].message}}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+        <v-divider></v-divider>
+        <!-- <v-card tile elevation="0" height="5%">
+          <v-list-item v-if="displaymail === -1"></v-list-item>
+          <v-list-item v-else>
+            <v-list-item-subtitle>Attachment</v-list-item-subtitle>
+          </v-list-item>
+        </v-card>-->
+      </v-card></v-slide-x-reverse-transition>
+      <v-card tile elevation="0" outlined height="93vh" v-if="ShowMailSet()" class="overflow-y-auto">
+        <v-card-title fixed>
           <v-text-field
             outlined
             append-icon="mdi-magnify"
@@ -64,84 +105,60 @@
           ></v-text-field>
         </v-card-title>
         <v-divider></v-divider>
-        <v-list 
-          two-line
-          v-for="(item, i) in mail[displayfolder].folder"
-          :key="item.time"
-          class="pa-0"
-        >
-          <v-list-item @click="Click(i)">
-            <v-list-item-avatar>
-              <v-img :src="item.avatar"></v-img>
-            </v-list-item-avatar>
-            <v-list-item-content>
-              <v-list-item-title>{{item.subject}}</v-list-item-title>
-              <v-list-item-subtitle>{{item.sender}} - {{item.time}}</v-list-item-subtitle>
-              <v-list-item-subtitle>{{item.content}}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider></v-divider>
-        </v-list>
-        <v-dialog v-model="contentDialog" id="dialog">
-          <v-card height="80vh">
-            <v-list-item three-line>
-              <!-- <v-list-item-avatar size="64px">
-                <v-img :src="mail[displayfolder].folder[displaymail].avatar"></v-img>
+        <v-container style="max-height: 100%;" class="overflow-x-auto pa-0">
+          <v-list style="width: 100%;" class="pa-0"><v-list-item-group>
+            <v-list-item 
+              two-line
+              v-for="(item, i) in mail[displayfolder].folder"
+              :key="item.time"
+              @click="Click(i)"
+            >
+              <v-list-item-avatar>
+                <v-img :src="item.avatar"></v-img>
               </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title class="headline">{{mail[displayfolder].folder[displaymail].sender}}</v-list-item-title>
-                <v-list-item-subtitle>{{mail[displayfolder].folder[displaymail].time}}</v-list-item-subtitle>
-              </v-list-item-content> -->
-              <v-spacer></v-spacer>
-              <v-tooltip bottom v-if="displayfolder != 1">
-                <template v-slot:activator="{ on }">
-                  <v-btn class="ml-1" icon outlined v-on="on">
-                    <v-icon @click="Reply(displayfolder,displaymail)">mdi-reply</v-icon>
-                  </v-btn>
-                </template>
-                <span>reply</span>
-              </v-tooltip>
-              <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                  <v-btn class="ml-1" icon outlined v-on="on" @click="Remove(displayfolder,displaymail)">
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </template>
-                <span>delete</span>
-              </v-tooltip>
-            </v-list-item>
-          </v-card>
-        </v-dialog>
-        <v-btn left bottom fixed fab class="text-center hidden-md-and-up" color="primary" @click="composeDialog = true">
-          <v-icon>mdi-pencil</v-icon>
-        </v-btn>
-        <v-speed-dial right fixed bottom v-model="speedDial" class="hidden-md-and-up">
-          <template v-slot:activator>
-            <v-btn v-model="speedDial" color="primary" dark fab>
-              <v-icon v-if="speedDial">mdi-close</v-icon>
-              <v-icon v-else>mdi-folder-open</v-icon>
-            </v-btn>
-          </template>
-          <v-btn fab small color="grey--text" @click="displayfolder = 1">
-            <v-icon>mdi-send</v-icon>
-          </v-btn>
-          <v-btn fab small color="grey--text" @click="displayfolder = 0">
-            <v-icon>mdi-inbox</v-icon>
-          </v-btn>
-        </v-speed-dial>
+              <v-list-item-content v-if="item.status === 1" class="font-weight-bold">
+                <v-list-item-title>{{item.subject}}</v-list-item-title>
+                <v-list-item-subtitle>{{item.sender}} - {{item.timestamp}}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{item.message}}</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-content v-else>
+                <v-list-item-title>{{item.subject}}</v-list-item-title>
+                <v-list-item-subtitle>{{item.sender}} - {{item.time}}</v-list-item-subtitle>
+                <v-list-item-subtitle>{{item.message}}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item></v-list-item-group>
+          </v-list>
+        </v-container>
       </v-card>
+      <v-btn left bottom fixed fab class="text-center hidden-md-and-up" color="primary" @click="composeDialog = true">
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+      <v-speed-dial right fixed bottom v-model="speedDial" class="hidden-md-and-up">
+        <template v-slot:activator>
+          <v-btn v-model="speedDial" color="primary" dark fab>
+            <v-icon v-if="speedDial">mdi-close</v-icon>
+            <v-icon v-else>mdi-folder-open</v-icon>
+          </v-btn>
+        </template>
+        <v-btn fab small color="grey--text" @click="Clear(1)">
+          <v-icon>mdi-send</v-icon>
+        </v-btn>
+        <v-btn fab small color="grey--text" @click="Clear(0)">
+          <v-icon>mdi-inbox</v-icon>
+        </v-btn>
+      </v-speed-dial>
     </v-col>
     <v-col cols="7" class="ma-0 hidden-sm-and-down">
       <v-card tile outlined height="93vh">
         <v-card tile elevation="0" height="20%" class="pt-3 px-3">
           <v-list-item v-if="displaymail === -1"></v-list-item>
-          <v-list-item v-else three-line>
+          <v-list-item v-else two-line>
             <v-list-item-avatar size="64px">
               <v-img :src="mail[displayfolder].folder[displaymail].avatar"></v-img>
             </v-list-item-avatar>
             <v-list-item-content>
               <v-list-item-title class="headline">{{mail[displayfolder].folder[displaymail].sender}}</v-list-item-title>
-              <v-list-item-subtitle>{{mail[displayfolder].folder[displaymail].time}}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{mail[displayfolder].folder[displaymail].timestamp}}</v-list-item-subtitle>
             </v-list-item-content>
             <v-spacer></v-spacer>
             <v-tooltip bottom v-if="displayfolder != 1">
@@ -178,12 +195,12 @@
           </v-list-item>
         </v-card>
         <v-divider></v-divider>
-        <v-card tile elevation="0" height="10%">
+        <!-- <v-card tile elevation="0" height="10%">
           <v-list-item v-if="displaymail === -1"></v-list-item>
           <v-list-item v-else>
             <v-list-item-subtitle>Attachment</v-list-item-subtitle>
           </v-list-item>
-        </v-card>
+        </v-card> -->
       </v-card>
     </v-col>
   </v-row>
@@ -200,7 +217,7 @@ export default {
       draftSubject: '',
       draftMessage: '',
       composeDialog: false,
-      contentDialog: false,
+      showMail: false,
       displayfolder: 0,
       displaymail: -1,
       speedDial: false,
@@ -213,58 +230,96 @@ export default {
           // inbox
           folder: [
             { 
+              status: 1,
               sender: 'Uier',
               avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
               subject: '資工營面試通知',
               message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
-              time: '2019/03/05 23:32'
+              timestamp: 201903052332
             },
             { 
+              status: 1,
               sender: 'Kelly',
               avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
               subject: 'test',
               message: '資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面試通知資工營面',
-              time: '2019/12/13 12:13'
+              timestamp: 201912131213
             },
             { 
+              status: 1,
               sender: 'ysl',
               avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-              subject: 'ooo',
+              subject: '測',
               message: '0991234567890qwer tyuioasdfghjklzxcvbnm,098765edkdftgyhunfd6csxexbabiubcuibgfwvgurefviuegrvfuervfuwrvfiwugvfuevfugrvfuwvrfuviugfevrugfvrguvurefviuegrvfuervfuwrvfiwugvfuevfugrvfuwvrfuviugfevrugfvrguvurefviuegrvfuervfuwrvfiwugvfuevfugrvfuwvrfuviugfevrugfvrguv',
-              time: '2019/05/11 05:11'
+              timestamp: 201905110511
             },
             { 
+              status: 1,
               sender: '黃欣欣',
               avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
               subject: '資工營面試通知',
               message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
-              time: '2019/11/02 11:02'
+              timestamp: 201911021102
             },
             { 
+              status: 1,
               sender: '臻臻',
               avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
               subject: '資工營面試通知',
               message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
-              time: '2019/04/06 04:06'
+              timestamp: 201904060406
             },
             { 
+              status: 1,
               sender: 'Ku Lily',
               avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
               subject: '資工營面試通知',
               message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
-              time: '2019/03/08 03:08'
+              timestamp: 201903080309
             },
-            
+            { 
+              status: 1,
+              sender: 'Ku Lily',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: '資工營面試通知',
+              message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              timestamp: 201903080312
+            },
+            { 
+              status: 1,
+              sender: 'Ku Lily',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: '資工營面試通知',
+              message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              timestamp: 201903080008
+            },
+            { 
+              status: 1,
+              sender: 'Lily',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: '資工營面試通知',
+              message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              timestamp: 201903088808
+            },
+            { 
+              status: 1,
+              sender: '黃欣欣',
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              subject: '資工營面試通知通知通知通知通知',
+              message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
+              timestamp: 201910021102
+            },
           ]
         },
         { // sent
           folder: [
             {
+              status: 1,
               sender: 'cpj',
               avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
               subject: 'iiiiiiiiii',
               message: 'As title, u! As title, u! As title, u! As title, u! As title, u!',
-              time: '2019/09/02 09:02'
+              timestamp: 201909020902
             }
           ]
         }
@@ -272,11 +327,18 @@ export default {
     }
   },
   methods: {
+    ShowMailSet() {
+      if(this.$vuetify.breakpoint.mdAndUp) return true;
+      else if(!this.showMail) return true;
+      else return false;
+    },
     Click(i) {
-      this.contentDialog = true
+      this.showMail = true
       this.displaymail = i
+      this.mail[this.displayfolder].folder[i].status = 0
     },
     Clear(i) {
+      this.showMail = false
       this.displayfolder = i
       this.displaymail = -1
     },
@@ -300,25 +362,4 @@ export default {
   }
 }
 </script>
-
-<style lang="css" scoped>
-
-@media screen and (max-width: 960px) {
-  #dialog {
-    visibility: visible;
-  }
-}
-
-@media screen and (min-width: 960px) and (max-width: 1904px) {
-  #dialog {
-    visibility: hidden;
-  }
-}
-
-@media screen and (min-width: 1904px) {
-  #dialog {
-    visibility: hidden;
-  }
-}
-
 </style>
