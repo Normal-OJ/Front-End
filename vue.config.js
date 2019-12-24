8e33e9da3fe51184803466a0b895aa5df25a646d
bogay <pojay11523@gmail.com>
2019-12-24 12:16:14 +0800

    pwa support

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,29 @@
+const manifestPlugin = require('pwa-manifest-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
   devServer: {
     disableHostCheck: true
+  },
+  configureWebpack: {
+    plugins: [
+      new manifestPlugin({
+        name: 'NOJ',
+        short_name: 'NOJ',
+        description: 'a normal online judge system.',
+        display: 'standalone',
+        start_url: '/',
+        orientation: 'portrait-primary',
+        icon: {
+          src: path.resolve('public/NOJ.png'),
+          sizes: [200]
+        },
+        background_color: '#003865',
+        theme_color: '#003865'
+      })
+    ]
   }
 }
