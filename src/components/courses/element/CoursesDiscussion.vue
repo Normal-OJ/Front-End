d5a29e2580ca2cab78e5d5034d4affe2e8c4cee5
鐘子淳 <40747030S@ntnu.edu.tw>
2019-12-26 01:25:56 +0800

    init discussion

--- src/components/courses/element/CoursesDiscussion.vue
+++ src/components/courses/element/CoursesDiscussion.vue
@@ -1,13 +1,157 @@
 <template>
+  <v-row no-gutters justify="center">
+    <v-card
+      class="my-6 flex-column d-flex"
+      elevation="6"
+      width="50vw"
+      v-for="(item, i) in post"
+      :key="i"
+    >
+      <v-card-subtitle>
+        Posted by <a>{{ item.author }}</a>, {{ item.createdTime }}.<br>
+          <i>Last update on {{ item.lastUpdatedTime }}.</i>
+      </v-card-subtitle>
+      <v-card-title class="headline ma-1">
+        <vue-markdown>{{item.title}}</vue-markdown>
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-list two-line>
+        <v-list-group
+          v-for="(msg, j) in item.comment"
+          :key="j"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-col cols="1" class="pa-0">
+              <v-avatar>
+                <v-img :src="msg.avatar"></v-img>
+              </v-avatar>
+              <v-list-item-title class="font-weight-medium">{{msg.name}}</v-list-item-title>
+            </v-col>
+            <v-col cols="9" class="pl-2">
+              <v-list-item-content class="py-0">
+                <vue-markdown>{{msg.message}}</vue-markdown>
+              </v-list-item-content>
+            </v-col>
+          </template>
+          <v-list-item
+            v-for="(submsg, k) in msg.subcomment"
+            :key="k"
+          >
+            <v-divider></v-divider>
+            <v-col cols="1" class="pa-0">
+              <v-avatar>
+                <v-img :src="submsg.avatar"></v-img>
+              </v-avatar>
+              <v-list-item-title class="font-weight-medium">{{submsg.name}}</v-list-item-title>
+            </v-col>
+            <v-col cols="10" class="pl-2" v-if="submsg.editComment == true">
+              <v-list-item-content class="py-0">
+                <v-textarea @submit.prevent="submsg.editComment = false" v-model="submsg.message" no-resize></v-textarea>
+              </v-list-item-content>
+            </v-col>
+            <v-col cols="10" class="pl-2" v-else>
+              <v-list-item-content class="py-0">
+                <vue-markdown>{{submsg.message}}</vue-markdown>
+              </v-list-item-content>
+            </v-col>
+            <v-col cols="1">
+              <v-menu offset-x>
+                <template v-slot:activator="{ on }">
+                  <v-btn icon v-on="on">
+                    <v-icon>mdi-dots-vertical</v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item
+                    v-for="(item, idx) in commentMenu"
+                    :key="idx"
+                    @click="submsg.editComment = true"
+                  >
+                    <v-list-item-title>{{item}}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-col>
+          </v-list-item>
+        </v-list-group>
+      </v-list>
+    </v-card>
+  </v-row>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
 export default {
 
   name: 'CoursesDiscussion',
 
+  components: {
+    VueMarkdown,
+  },
+
   data () {
     return {
+      newComment: '',
+      newSubComment: '',
+      commentMenu: [
+        'edit',
+        'delete',
+      ],
+      post: [
+        {
+          title: 'Want to eat something...',
+          author: 'fuji',
+          createdTime: '2019/12/10 13:59',
+          lastUpdatedTime: '2019/12/24 21:21',
+          comment: [
+            {
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+              name: '七大卡',
+              message: '~~7 kcal~~',
+              subcomment: [
+                {
+                  avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+                  name: '八卡',
+                  message: '8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal 8 cal ',
+                  editComment: false,
+                },
+                {
+                  avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+                  name: 'fuji',
+                  message: '-43',
+                  editComment: false,
+                },
+                {
+                  avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                  name: '九大卡',
+                  message: '9 kcal',
+                  editComment: false,
+                },
+              ]
+            },
+            {
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+              name: '七卡',
+              message: 'fujiwara',
+              subcomment: [
+              ]
+            }
+          ]
+        },
+        {
+          title: '**test** *test*',
+          author: 'jkdf',
+          createdTime: '2019/12/10 04:00',
+          lastUpdatedTime: '2019/12/24 21:21',
+          comment: [
+          ]
+        }
+      ],
+    }
+  },
+  methods: {
+    Submit() {
 
     }
   }
