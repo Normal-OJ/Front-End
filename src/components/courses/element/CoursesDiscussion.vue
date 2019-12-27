dee040f5cafa4db42f74146f02e12cae552b1bae
鐘子淳 <40747030S@ntnu.edu.tw>
2019-12-27 22:28:54 +0800

    some interface adjustment

--- src/components/courses/element/CoursesDiscussion.vue
+++ src/components/courses/element/CoursesDiscussion.vue
@@ -14,23 +14,35 @@
       <v-card-title class="headline ma-1">
         <vue-markdown>{{item.title}}</vue-markdown>
       </v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="primary"
+          class="text-none subtitle-1"
+          text
+          outlined
+          @click="showComments = !showComments"
+        >Comment</v-btn>
+      </v-card-actions>
       <v-divider></v-divider>
-      <!-- Comment -->
-      <v-list two-line expand>
+      <v-list two-line expand class="pa-0" v-if="showComments">
+        <!-- Comments With Subcomment -->
         <v-list-group
           v-for="(msg, j) in item.comment"
+          v-if="msg.subcomment.length !== 0"
           :key="j"
           no-action
+          class="pt-1"
         >
-          <template v-slot:activator>
+          <template v-slot:activator> 
             <v-col cols="1" class="pa-0">
-              <v-avatar>
+              <v-avatar class="ml-1">
                 <v-img :src="msg.avatar"></v-img>
               </v-avatar>
-              <v-list-item-title class="font-weight-medium">{{msg.name}}</v-list-item-title>
+              <v-list-item-title class="font-weight-medium text-center">{{msg.name}}</v-list-item-title>
             </v-col>
             <!-- Edit Message -->
-            <v-col cols="9" class="pl-2" v-if="msg.editComment == true">
+            <v-col cols="10" class="pl-2" v-if="msg.editComment == true">
               <v-list-item-content class="py-0">
                 <v-textarea
                   @keydown="inputHandler($event,i,j,-1)"
@@ -39,16 +51,17 @@
                   no-resize
                   auto-grow
                   rows="1"
+                  autofocus
                 ></v-textarea>
               </v-list-item-content>
             </v-col>
-            <v-col cols="9" class="pl-2" v-else>
+            <v-col cols="10" class="pl-2" v-else>
               <v-list-item-content class="py-0">
                 <vue-markdown>{{msg.message}}</vue-markdown>
               </v-list-item-content>
             </v-col>
             <!-- More Option Menu -->
-<!--             <v-col cols="1">
+            <v-col cols="1">
               <v-menu offset-x>
                 <template v-slot:activator="{ on }">
                   <v-btn icon v-on="on">
@@ -59,26 +72,24 @@
                   <v-list-item
                     v-for="(item, idx) in commentMenu"
                     :key="idx"
-                    @click="msg.editComment = true"
+                    @click="optionMenu(idx, i, j)"
                   >
                     <v-list-item-title>{{item}}</v-list-item-title>
                   </v-list-item>
                 </v-list>
               </v-menu>
-            </v-col> -->
+            </v-col>
           </template>
           <!-- Subcomment -->
           <v-list-item
             v-for="(submsg, k) in msg.subcomment"
             :key="k"
           >
-            <v-divider></v-divider>
-
             <v-col cols="1" class="pa-0">
-              <v-avatar>
+              <v-avatar class="ml-1">
                 <v-img :src="submsg.avatar"></v-img>
               </v-avatar>
-              <v-list-item-title class="font-weight-medium">{{submsg.name}}</v-list-item-title>
+              <v-list-item-title class="font-weight-medium text-center">{{submsg.name}}</v-list-item-title>
             </v-col>
             <!-- Edit SubMessage -->
             <v-col cols="10" class="pl-2" v-if="submsg.editComment == true">
@@ -90,6 +101,7 @@
                   no-resize
                   auto-grow
                   rows="1"
+                  autofocus
                 ></v-textarea>
               </v-list-item-content>
             </v-col>
@@ -120,6 +132,74 @@
             </v-col>
           </v-list-item>
         </v-list-group>
+        <!-- Comments Without Subcomment -->
+        <v-list-item v-else class="pt-1">
+          <v-col cols="1" class="pa-0">
+            <v-avatar class="ml-1">
+              <v-img :src="msg.avatar"></v-img>
+            </v-avatar>
+            <v-list-item-title class="font-weight-medium text-center">{{msg.name}}</v-list-item-title>
+          </v-col>
+          <!-- Edit Message -->
+          <v-col cols="10" class="pl-2" v-if="msg.editComment == true">
+            <v-list-item-content class="py-0">
+              <v-textarea
+                @keydown="inputHandler($event,i,j,-1)"
+                @blur="msg.editComment = false"
+                v-model="msg.message"
+                no-resize
+                auto-grow
+                rows="1"
+                autofocus
+              ></v-textarea>
+            </v-list-item-content>
+          </v-col>
+          <v-col cols="10" class="pl-2" v-else>
+            <v-list-item-content class="py-0">
+              <vue-markdown>{{msg.message}}</vue-markdown>
+            </v-list-item-content>
+          </v-col>
+          <!-- More Option Menu -->
+          <v-col cols="1">
+            <v-menu offset-x>
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on">
+                  <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="(item, idx) in commentMenu"
+                  :key="idx"
+                  @click="optionMenu(idx, i, j)"
+                >
+                  <v-list-item-title>{{item}}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-col>
+        </v-list-item>
+        <v-list-item>
+          <v-col cols="1" class="pa-0">
+            <v-avatar class="ml-1">
+              <v-img :src="require('@/assets/defaultAvatar.png')"></v-img>
+            </v-avatar>
+          </v-col>
+          <!-- Edit Message -->
+          <v-col cols="10" class="pl-2">
+            <v-list-item-content class="py-0">
+              <v-textarea
+                @keydown="inputHandler($event,i,-1,-1)"
+                v-model="newComment"
+                no-resize
+                auto-grow
+                rows="1"
+                autofocus
+                label="Write some comment..."
+              ></v-textarea>
+            </v-list-item-content>
+          </v-col>
+        </v-list-item>
       </v-list>
     </v-card>
   </v-row>
@@ -139,7 +219,9 @@ export default {
     return {
       newComment: '',
       newSubComment: '',
+      showComments: false,
       commentMenu: [
+        'reply',
         'edit',
         'delete',
       ],
@@ -155,6 +237,7 @@ export default {
               avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
               name: '七大卡',
               message: '~~7 kcal~~',
+              editComment: false,
               subcomment: [
                 {
                   avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
@@ -181,8 +264,31 @@ export default {
               name: '七卡',
               message: 'fujiwara',
               subcomment: [
-              ]
-            }
+              ],
+              editComment: false,
+            },
+            {
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+              name: '七',
+              message: '7',
+              subcomment: [
+                {
+                  avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+                  name: '九大卡',
+                  message: '9 kcal',
+                  editComment: false,
+                },
+              ],
+              editComment: false,
+            },
+            {
+              avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+              name: '千花',
+              message: 'chika',
+              subcomment: [
+              ],
+              editComment: false,
+            },
           ]
         },
         {
@@ -201,11 +307,18 @@ export default {
     inputHandler(e,i,j,k) {
       if (e.keyCode === 13) {
         if(!e.shiftKey) {
-          if(k != -1) this.post[i].comment[j].subcomment[k].editComment = false;
+          if(j === -1) {
+            this.post[i].comment.push({'avatar': '', 'name': 'fuji', 'message': this.newComment, 'subcomment': [], 'editComment': false})
+            this.newComment = ''
+          }
+          else if(k !== -1) this.post[i].comment[j].subcomment[k].editComment = false;
           else this.post[i].comment[j].editComment = false;
           // this.editComment()
         }
       }
+    },
+    optionMenu(idx,i,j) {
+      if(idx === 1) this.post[i].comment[j].editComment = true
     }
   }
 }
