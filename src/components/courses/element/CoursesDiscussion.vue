89a4936cd369d9e82a5a4e88547d9881f6c1b846
鐘子淳 <40747030S@ntnu.edu.tw>
2019-12-27 15:24:11 +0800

    allow edit message

--- src/components/courses/element/CoursesDiscussion.vue
+++ src/components/courses/element/CoursesDiscussion.vue
@@ -15,7 +15,8 @@
         <vue-markdown>{{item.title}}</vue-markdown>
       </v-card-title>
       <v-divider></v-divider>
-      <v-list two-line>
+      <!-- Comment -->
+      <v-list two-line expand>
         <v-list-group
           v-for="(msg, j) in item.comment"
           :key="j"
@@ -28,33 +29,77 @@
               </v-avatar>
               <v-list-item-title class="font-weight-medium">{{msg.name}}</v-list-item-title>
             </v-col>
-            <v-col cols="9" class="pl-2">
+            <!-- Edit Message -->
+            <v-col cols="9" class="pl-2" v-if="msg.editComment == true">
+              <v-list-item-content class="py-0">
+                <v-textarea
+                  @keydown="inputHandler($event,i,j,-1)"
+                  @blur="msg.editComment = false"
+                  v-model="msg.message"
+                  no-resize
+                  auto-grow
+                  rows="1"
+                ></v-textarea>
+              </v-list-item-content>
+            </v-col>
+            <v-col cols="9" class="pl-2" v-else>
               <v-list-item-content class="py-0">
                 <vue-markdown>{{msg.message}}</vue-markdown>
               </v-list-item-content>
             </v-col>
+            <!-- More Option Menu -->
+<!--             <v-col cols="1">
+              <v-menu offset-x>
+                <template v-slot:activator="{ on }">
+                  <v-btn icon v-on="on">
+                    <v-icon>mdi-dots-vertical</v-icon>
+                  </v-btn>
+                </template>
+                <v-list>
+                  <v-list-item
+                    v-for="(item, idx) in commentMenu"
+                    :key="idx"
+                    @click="msg.editComment = true"
+                  >
+                    <v-list-item-title>{{item}}</v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+            </v-col> -->
           </template>
+          <!-- Subcomment -->
           <v-list-item
             v-for="(submsg, k) in msg.subcomment"
             :key="k"
           >
             <v-divider></v-divider>
+
             <v-col cols="1" class="pa-0">
               <v-avatar>
                 <v-img :src="submsg.avatar"></v-img>
               </v-avatar>
               <v-list-item-title class="font-weight-medium">{{submsg.name}}</v-list-item-title>
             </v-col>
+            <!-- Edit SubMessage -->
             <v-col cols="10" class="pl-2" v-if="submsg.editComment == true">
               <v-list-item-content class="py-0">
-                <v-textarea @submit.prevent="submsg.editComment = false" v-model="submsg.message" no-resize></v-textarea>
+                <v-textarea
+                  @keydown="inputHandler($event,i,j,k)"
+                  @blur="submsg.editComment = false"
+                  v-model="submsg.message"
+                  no-resize
+                  auto-grow
+                  rows="1"
+                ></v-textarea>
               </v-list-item-content>
             </v-col>
+            <!-- SubMessage -->
             <v-col cols="10" class="pl-2" v-else>
               <v-list-item-content class="py-0">
                 <vue-markdown>{{submsg.message}}</vue-markdown>
               </v-list-item-content>
             </v-col>
+            <!-- More Option Menu -->
             <v-col cols="1">
               <v-menu offset-x>
                 <template v-slot:activator="{ on }">
@@ -104,6 +149,7 @@ export default {
           author: 'fuji',
           createdTime: '2019/12/10 13:59',
           lastUpdatedTime: '2019/12/24 21:21',
+          editComment: false,
           comment: [
             {
               avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
@@ -144,6 +190,7 @@ export default {
           author: 'jkdf',
           createdTime: '2019/12/10 04:00',
           lastUpdatedTime: '2019/12/24 21:21',
+          editComment: false,
           comment: [
           ]
         }
@@ -151,8 +198,14 @@ export default {
     }
   },
   methods: {
-    Submit() {
-
+    inputHandler(e,i,j,k) {
+      if (e.keyCode === 13) {
+        if(!e.shiftKey) {
+          if(k != -1) this.post[i].comment[j].subcomment[k].editComment = false;
+          else this.post[i].comment[j].editComment = false;
+          // this.editComment()
+        }
+      }
     }
   }
 }
