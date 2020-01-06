21631408f40e42ac990ec445b5f31ad9acc94c31
shangchiwu <shangchi537@gmail.com>
2020-01-06 22:11:14 +0800

    remove hardcoded test announcement

--- src/components/home/Announcement.vue
+++ src/components/home/Announcement.vue
@@ -68,8 +68,8 @@ export default {
   },
 
   beforeMount () {
-    // this.getAnnouncements();
-    this.useFakeData();
+    this.getAnnouncements();
+    // this.useFakeData();
   },
 
   methods: {
@@ -112,49 +112,49 @@ export default {
         });
     },
 
-    useFakeData() {
-      let ann = [
-        {
-          annId: 1,
-          title: 'Welcome',
-          creater: 'sys',
-          markdown: '一级標題\n=======\n## 二级標題\n段落以空白行分隔。\n行末兩個空格  産生斷行。\n文本屬性：_斜體_、\n**粗體**、`等寬字型Monospace`。\n水平線：\n---\n列表：\n  * 張三\n  * 李四\n  * 王二\n編號列表：\n  1. 不論\n  2. 三七\n  3. 二十一\n[連結][示例]。\n  [示例]: http://example.com\n![圖](Icon-pictures.png "icon")\n> Markdown使用電郵用字元「>」來引用。\n行間<abbr title="Hypertext Markup Language">HTML</abbr>亦受支援。',
-          createTime: 1578315258,
-          updateTime: 1578315258,
-          updater: 'sys4'
-        },
-        {
-          annId: 2,
-          title: 'Welcome 2',
-          creater: 'sys2',
-          markdown: 'Wellcome BBBBBYou!!\nWellcome You!!\nWellcome You!!\nWellcome You!!\n',
-          createTime: 1578330258,
-          updateTime: 1578330258,
-          updater: 'sys5'
-        },
-        {
-          annId: 3,
-          title: 'Welcome 3',
-          creater: 'sys3',
-          markdown: 'Wellcome CCCCCYou!!\nWellcome You!!\nWellcome You!!\nWellcome You!!\n',
-          createTime: 1578360258,
-          updateTime: 1578390258,
-          updater: 'sys6'
-        },
-      ];
-      console.log(ann);
-      ann.forEach(ele => {
-        this.items.push({
-          'annId': ele.annId,
-          'title': ele.title,
-          'author': ele.creater,
-          'content': ele.markdown,
-          'createdTime': this.timeFormat(ele.createTime),
-          'lastUpdatedTime': this.timeFormat(ele.updateTime),
-          'lastUpdater': ele.updater});
-      });
-      console.log(this.items);
-    }
+    // useFakeData() {
+    //   let ann = [
+    //     {
+    //       annId: 1,
+    //       title: 'Welcome',
+    //       creater: 'sys',
+    //       markdown: '一级標題\n=======\n## 二级標題\n段落以空白行分隔。\n行末兩個空格  産生斷行。\n文本屬性：_斜體_、\n**粗體**、`等寬字型Monospace`。\n水平線：\n---\n列表：\n  * 張三\n  * 李四\n  * 王二\n編號列表：\n  1. 不論\n  2. 三七\n  3. 二十一\n[連結][示例]。\n  [示例]: http://example.com\n![圖](Icon-pictures.png "icon")\n> Markdown使用電郵用字元「>」來引用。\n行間<abbr title="Hypertext Markup Language">HTML</abbr>亦受支援。',
+    //       createTime: 1578315258,
+    //       updateTime: 1578315258,
+    //       updater: 'sys4'
+    //     },
+    //     {
+    //       annId: 2,
+    //       title: 'Welcome 2',
+    //       creater: 'sys2',
+    //       markdown: 'Wellcome BBBBBYou!!\nWellcome You!!\nWellcome You!!\nWellcome You!!\n',
+    //       createTime: 1578330258,
+    //       updateTime: 1578330258,
+    //       updater: 'sys5'
+    //     },
+    //     {
+    //       annId: 3,
+    //       title: 'Welcome 3',
+    //       creater: 'sys3',
+    //       markdown: 'Wellcome CCCCCYou!!\nWellcome You!!\nWellcome You!!\nWellcome You!!\n',
+    //       createTime: 1578360258,
+    //       updateTime: 1578390258,
+    //       updater: 'sys6'
+    //     },
+    //   ];
+    //   console.log(ann);
+    //   ann.forEach(ele => {
+    //     this.items.push({
+    //       'annId': ele.annId,
+    //       'title': ele.title,
+    //       'author': ele.creater,
+    //       'content': ele.markdown,
+    //       'createdTime': this.timeFormat(ele.createTime),
+    //       'lastUpdatedTime': this.timeFormat(ele.updateTime),
+    //       'lastUpdater': ele.updater});
+    //   });
+    //   console.log(this.items);
+    // }
   }
 }
 </script>
