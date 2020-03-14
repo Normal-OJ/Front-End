4d38b8d884fde39ac3c74aef861257519e6fac19
Uier <ap9940506@gmail.com>
2020-03-15 02:44:11 +0800

    reduce button nav elevation

--- src/pages/courses/manages/Manages.vue
+++ src/pages/courses/manages/Manages.vue
@@ -4,6 +4,10 @@
       <v-bottom-navigation
         v-model="activeBtn"
         color="primary"
+        style="
+          -webkit-box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12) !important;
+          box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12) !important;
+        "
         grow
       >
         <v-btn value="students">
